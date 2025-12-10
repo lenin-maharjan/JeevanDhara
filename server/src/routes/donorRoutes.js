0ac7a8f76ce3245139e0946ee88db44f7cc04e1f
Lenin Maharjan <leninmaharjan123@gmail.com>
2025-12-10 19:16:21 +0545

    Fix: Remove legacy register/login routes from donorRoutes to prevent crash

--- server/src/routes/donorRoutes.js
+++ server/src/routes/donorRoutes.js
@@ -1,18 +1,13 @@
-const express = require('express');
-const { 
-  registerDonor, 
-  loginDonor, 
-  getAllDonors, 
-  getDonorById, 
-  updateDonor, 
-  deleteDonor, 
-  searchDonors 
+const {
+  getAllDonors,
+  getDonorById,
+  updateDonor,
+  deleteDonor,
+  searchDonors
 } = require('../controllers/donorController');
 
 const router = express.Router();
 
-router.post('/register', registerDonor);
-router.post('/login', loginDonor);
 router.get('/', getAllDonors);
 router.get('/search', searchDonors);
 router.get('/:id', getDonorById);
